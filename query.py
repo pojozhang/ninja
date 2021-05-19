ddbdbbe95cc8860dc005c421475f5c7aca11bdc0
Tony <pojo.zhang@gmail.com>
2021-05-19 23:12:26 +0800

    增加查询k线功能

--- query.py
+++ query.py
@@ -20,6 +20,15 @@ def query_ticker(args):
     return client.ticker(inst_id=args.instId)
 
 
+def query_candles(args):
+    candles = client.candles(inst_id=args.instId, bar=args.bar, limit=args.limit)
+    list = candles
+    while len(candles) > 0 and len(list) < args.limit:
+        candles = client.candles(inst_id=args.instId, bar=args.bar, after=candles[0][0])
+        list.extend(candles)
+    return list
+
+
 if __name__ == '__main__':
     logging.config.fileConfig('log.conf')
     logger = logging.getLogger(__name__)
@@ -41,10 +50,13 @@ if __name__ == '__main__':
     ticker.set_defaults(func=query_ticker)
 
     candles = subparsers.add_parser("candles", parents=[common])
-    candles.add_argument('--instType', type=str, default='SPOT', choices=['SPOT', 'SWAP', 'FUTURES', 'OPTION'])
-    candles.add_argument('--uly', type=str)
+    candles.add_argument('--bar', type=str, default='15m',
+                         choices=['1m', '3m', '5m', '15m', '30m',
+                                  '1H', '2H', '4H', '6H', '12H',
+                                  '1D', '1W', '1M', '3M', '6M', '1Y'])
     candles.add_argument('--instId', type=str)
-    candles.set_defaults(func=query_instruments)
+    candles.add_argument('--limit', type=int, default=100)
+    candles.set_defaults(func=query_candles)
 
     args = parser.parse_args()
     data = json.dumps(args.func(args), indent=4)
