539821cea7f6918db61c677f4e8644c620fefe25
Tony <pojo.zhang@gmail.com>
2021-05-19 22:08:05 +0800

    增加查询结果输出到文件功能

--- query.py
+++ query.py
@@ -1,9 +1,9 @@
 import argparse
 import configparser
+import json
 import logging.config
 
 import okex
-from utils import print_json
 
 cp = configparser.ConfigParser()
 cp.read("config.ini")
@@ -13,28 +13,45 @@ client = okex.Client(config)
 
 
 def query_instruments(args):
-    print_json(client.instruments(inst_type=args.instType))
+    return client.instruments(inst_type=args.instType)
 
 
 def query_ticker(args):
-    print_json(client.ticker(inst_id=args.instId))
+    return client.ticker(inst_id=args.instId)
 
 
 if __name__ == '__main__':
     logging.config.fileConfig('log.conf')
+    logger = logging.getLogger(__name__)
+
+    common = argparse.ArgumentParser(add_help=False)
+    common.add_argument('--saveas', type=str, help='write data to a file')
 
     parser = argparse.ArgumentParser(description='Query information.')
     subparsers = parser.add_subparsers()
 
-    instruments = subparsers.add_parser("instruments")
+    instruments = subparsers.add_parser("instruments", parents=[common])
     instruments.add_argument('--instType', type=str, default='SPOT', choices=['SPOT', 'SWAP', 'FUTURES', 'OPTION'])
     instruments.add_argument('--uly', type=str)
     instruments.add_argument('--instId', type=str)
     instruments.set_defaults(func=query_instruments)
 
-    ticker = subparsers.add_parser("ticker")
+    ticker = subparsers.add_parser("ticker", parents=[common])
     ticker.add_argument('--instId', type=str)
     ticker.set_defaults(func=query_ticker)
 
+    candles = subparsers.add_parser("candles", parents=[common])
+    candles.add_argument('--instType', type=str, default='SPOT', choices=['SPOT', 'SWAP', 'FUTURES', 'OPTION'])
+    candles.add_argument('--uly', type=str)
+    candles.add_argument('--instId', type=str)
+    candles.set_defaults(func=query_instruments)
+
     args = parser.parse_args()
-    args.func(args)
+    data = json.dumps(args.func(args), indent=4)
+
+    if args.saveas:
+        file = open(args.saveas, 'w')
+        file.write(data)
+        file.close()
+    else:
+        logger.info(data)
